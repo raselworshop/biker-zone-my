f5cbf194bd6879b602fcde728330f7eb0b9044c1
raselworshop <raselworshop@gmail.com>
2024-08-20 12:33:52 -0400

    added changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['*'],
   theme: {
+    fontFamily: {
+      'poppins': ['"Poppins"', '"sans-serif"'],
+    },
     extend: {},
   },
   plugins: [require('daisyui'),],
